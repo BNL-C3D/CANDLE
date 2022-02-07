da52a87a4d9bb13be95613e9040dafbac6fa90d4
darkwhite29 <darkwhite29@gmail.com>
2022-02-07 10:33:09 -0500

    added a new test of varying task TTX

---
+++ setup.sh
@@ -0,0 +1,14 @@
+. /ccs/home/litan/miniconda3/etc/profile.d/conda.sh
+conda activate ddmd_async
+
+#export RMQ_PASSWORD=RqQE5Gc72eqndyGh
+#export RMQ_USERNAME=wf_ucl
+#export RMQ_HOSTNAME=apps.marble.ccs.ornl.gov
+#export RMQ_PORT=30954
+#export RADICAL_PILOT_DBURL=mongodb://wf_ucl:RqQE5Gc72eqndyGh@apps.marble.ccs.ornl.gov:32622/wf_ucl_db
+
+export RMQ_PASSWORD=password
+export RMQ_USERNAME=admin
+export RMQ_HOSTNAME=apps.marble.ccs.ornl.gov
+export RMQ_PORT=31848
+export RADICAL_PILOT_DBURL=mongodb://rct:rct_test@apps.marble.ccs.ornl.gov:32020/rct_test
